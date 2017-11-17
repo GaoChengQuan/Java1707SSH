e14583559217b5027b15149a0723c57704199599
GaoChengQuan <529390053@qq.com>
2017-11-17 12:10:53 +0800

    ssh封装

--- src/main/java/com/situ/ssh/controller/base/BaseAction.java
+++ src/main/java/com/situ/ssh/controller/base/BaseAction.java
@@ -1,21 +1,37 @@
 package com.situ.ssh.controller.base;
 
+import java.io.IOException;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts2.ServletActionContext;
+import org.hibernate.criterion.DetachedCriteria;
 
 import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.ModelDriven;
 import com.situ.ssh.pojo.Admin;
+import com.situ.ssh.util.PageBean;
+
+import net.sf.json.JSONObject;
+import net.sf.json.JsonConfig;
 
 public class BaseAction<T> extends ActionSupport implements ModelDriven<T> {
+	protected PageBean pageBean = new PageBean();
 
 	//protect Student student;
 	protected T model;
+	//创建离线查询对象
+	protected DetachedCriteria detachedCriteria = null;
 
 	@Override
 	public T getModel() {
 		return model;
 	}
+	
+	private int page;//第几页
+	private int rows;//每页多少数据
 
 	public BaseAction() {
 		// this:当前运行的类(AdminDao/StudentDao)
@@ -28,6 +44,8 @@ public class BaseAction<T> extends ActionSupport implements ModelDriven<T> {
 		Type[] actualTypeArguments = superClass.getActualTypeArguments();// [Admin.class]
 		// 获取数组中第一个元素
 		Class<T> entityClass = (Class<T>) actualTypeArguments[0];
+		detachedCriteria = DetachedCriteria.forClass(entityClass);
+		pageBean.setDetachedCriteria(detachedCriteria);
 		try {
 			model = entityClass.newInstance();
 		} catch (InstantiationException e) {
@@ -36,5 +54,45 @@ public class BaseAction<T> extends ActionSupport implements ModelDriven<T> {
 			e.printStackTrace();
 		}
 	}
+	
+	/**
+	 * 将任意对象转换成json返回给浏览器
+	 * @param object
+	 */
+	public void obj2Json(Object object, String... excludes) {
+		// 将pageBean转换成json
+		JsonConfig jsonConfig = new JsonConfig();
+		//指定哪些属性不进行json转换
+		jsonConfig.setExcludes(excludes);
+		String json = JSONObject.fromObject(object, jsonConfig).toString();
+		HttpServletResponse response = ServletActionContext.getResponse();
+		response.setContentType("text/json;charset=utf-8");
+		try {
+			response.getWriter().println(json);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void obj2JsonForEasyUI(Object object) {
+		obj2Json(object, new String[]{"currentPage", "detachedCriteria", "pageSize"});
+	}
 
+	public int getPage() {
+		return page;
+	}
+
+	public void setPage(int page) {
+		this.page = page;
+		pageBean.setCurrentPage(page);
+	}
+
+	public int getRows() {
+		return rows;
+	}
+
+	public void setRows(int rows) {
+		this.rows = rows;
+		pageBean.setPageSize(rows);
+	}
 }
--- src/main/java/com/situ/ssh/dao/base/impl/BaseDaoImpl.java
+++ src/main/java/com/situ/ssh/dao/base/impl/BaseDaoImpl.java
@@ -8,9 +8,13 @@ import java.util.List;
 import javax.annotation.Resource;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Projections;
+import org.springframework.expression.spel.ast.Projection;
 import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
 
 import com.situ.ssh.dao.base.IBaseDao;
+import com.situ.ssh.util.PageBean;
 
 public class BaseDaoImpl<T> extends HibernateDaoSupport implements IBaseDao<T>{
 	//代表当前操作的实体类的类型 Student.class  Admin.class
@@ -62,6 +66,27 @@ public class BaseDaoImpl<T> extends HibernateDaoSupport implements IBaseDao<T>{
 		// "FROM student"
 		String sql = "FROM " + entityClass.getSimpleName();
 		return (List<T>) getHibernateTemplate().find(sql);
+	}
+
+	@Override
+	public void pageQuery(PageBean pageBean) {
+		Integer currentPage = pageBean.getCurrentPage();
+		Integer pageSize = pageBean.getPageSize();
+		DetachedCriteria detachedCriteria = pageBean.getDetachedCriteria();
+		
+		//查询total:总记录数
+		detachedCriteria.setProjection(Projections.rowCount());
+		List<?> countList = getHibernateTemplate().findByCriteria(detachedCriteria);
+		long total = (long) countList.get(0);
+		pageBean.setTotal((int)total);
+		
+		//rows:当前页数据
+		detachedCriteria.setProjection(null);
+		int firstResult = (currentPage - 1) * pageSize;
+		List<?> rows = getHibernateTemplate().findByCriteria(detachedCriteria, firstResult, pageSize);
+		//String sql = "FROM Student limit ?,? ";
+		//getHibernateTemplate().find(sql, firstResult, pageSize);
+		pageBean.setRows(rows);
 	}
 
 }
